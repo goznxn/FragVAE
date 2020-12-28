8615677ef5ab7ab88ff471655343399ecb0920ff
Zhenxiang Gao <49001224+goznxn@users.noreply.github.com>
2020-12-28 14:47:45 -0500

    Add files via upload

--- mol_generation.py
+++ mol_generation.py
@@ -10,18 +10,20 @@ import argparse
 from tqdm import tqdm
 
 #from hgraph import *
-from hgraph import *
+from model import *
 import rdkit
 
 lg = rdkit.RDLogger.logger() 
 lg.setLevel(rdkit.RDLogger.CRITICAL)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--vocab', required=True)
+parser.add_argument('--frag', required=True)
 parser.add_argument('--bond', required=True)
 parser.add_argument('--atom_vocab', default=common_atom_vocab)
-parser.add_argument('--model', required=True)
+parser.add_argument('--model_folder', required=True)
 parser.add_argument('--fragment_smiles', required=True)
+parser.add_argument('--radius', required=True)
+parser.add_argument('--mol_file', required=True)
 
 parser.add_argument('--topp', type=float, default=0.95)
 parser.add_argument('--num_decode', type=int, default=1)
@@ -38,7 +40,7 @@ parser.add_argument('--fragment_size', type=int, default=50)
 parser.add_argument('--hidden_size', type=int, default=250)
 parser.add_argument('--embed_size', type=int, default=250)
 parser.add_argument('--batch_size', type=int, default=1)
-parser.add_argument('--latent_size', type=int, default=24)
+parser.add_argument('--latent_size', type=int, default=250)
 parser.add_argument('--depthT', type=int, default=20)
 parser.add_argument('--depthG', type=int, default=20)
 parser.add_argument('--diterT', type=int, default=1)
@@ -50,31 +52,25 @@ args.enum_root = True
 args.greedy = not args.sample
 
 #args.test = [line.strip("\r\n ") for line in open(args.test)]
-with open(args.vocab) as f1:
+with open(args.frag) as f1:
     vocab = [x.strip("\r\n ").split() for x in f1]
 with open(args.bond) as f2:
     bond = [x.strip("\r\n ").split() for x in f2]
     
-args.vocab = DVocab(vocab, bond) 
+args.frag = DVocab(vocab, bond) 
 
-#if args.novi:
-#    model = HierGNN(args).cuda()
-#else:
-#    model = HierVGNN(args).cuda()
+model = MultiVAE(args).cuda()
 
-model = HierVAE(args).cuda()
-
-model.load_state_dict(torch.load(args.model))
+model.load_state_dict(torch.load(args.model_folder))
 model.eval()
 
-#dataset = MolEnumRootDataset(args.test, args.vocab, args.atom_vocab, 0)
-#loader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0, collate_fn=lambda x:x[0])
-#print(loader)
 torch.manual_seed(args.seed)
 random.seed(args.seed)
 
 with torch.no_grad(): 
+    f1=open(args.mol_file,'a')
     for _ in tqdm(range( args.num_decode // args.mol_num)):
-        smiles_list = model.singlesample(args.fragment_smiles,args.batch_size, args.beam_size, args.topp, args.mol_num, args.enum_root, args.fragment_num, args.greedy)
+        smiles_list = model.sample(args.fragment_smiles, args.radius, args.batch_size, args.beam_size, args.topp, args.mol_num, args.enum_root, args.fragment_num, args.greedy)
         for _,smiles in enumerate(smiles_list):
-            print(smiles)
+            f1.write(smiles+'\n')
+    f1.close()
